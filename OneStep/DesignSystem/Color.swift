7c95f1e4dd7fdc3086f811d13e558e62bf3d7923
Lucid <bluecho97@gmail.com>
2025-08-14 20:25:09 +0900

    NewProject

---
+++ OneStep/DesignSystem/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  OneStep
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import SwiftUI
+
+struct Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color()
+}
---
+++ OneStep/DesignSystem/Font.swift
@@ -0,0 +1,18 @@
+//
+//  Font.swift
+//  OneStep
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import SwiftUI
+
+struct Font: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Font()
+}
