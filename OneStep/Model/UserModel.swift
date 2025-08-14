f201bedc29096b49f76da3d37de4507fbd2dd1ee
Lucid <bluecho97@gmail.com>
2025-08-14 20:45:21 +0900

    GnerateModelFile

---
+++ OneStep/Model/UserModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserModel.swift
+//  OneStep
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import SwiftUI
+
+struct UserModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserModel()
+}
