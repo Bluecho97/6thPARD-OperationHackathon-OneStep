8b589ef08ad29df3778d954d011868f8a55aef44
Lucid <bluecho97@gmail.com>
2025-08-14 20:43:35 +0900

    GenerateFolder

---
+++ OneStep/ViewModel/HomeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModel.swift
+//  OneStep
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewModel()
+}
