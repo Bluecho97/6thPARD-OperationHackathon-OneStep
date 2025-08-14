ba902911c0dfe3d41fe906f04699eeef4e9fb99d
Lucid <bluecho97@gmail.com>
2025-08-14 20:15:36 +0900

    Initial Commit

---
+++ OneStep/OneStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepApp.swift
+//  OneStep
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct OneStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneStepTests/OneStepTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepTests.swift
+//  OneStepTests
+//
+//  Created by Lucid on 8/14/25.
+//
+
+import Testing
+@testable import OneStep
+
+struct OneStepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
